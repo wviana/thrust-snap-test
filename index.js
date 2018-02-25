4653be5cbf8488ce4023405bb194d31913f649f4
Wesley Viana <viana.wesley@gmail.com>
2018-02-25 01:06:04 -0300

    Interceptador agora trabalha com objeto export do amd

--- index.js
+++ index.js
@@ -2,21 +2,28 @@ const fs = require('filesystem')
 
 const interceptado = {}
 
+function interceptFunction(fn) {
+    return function() {
+        let retorno = fn.apply(this, arguments);
+        interceptado[fn.name] = interceptado[fn.name] || []
+        interceptado[fn.name].push({ entrada: arguments, saida: retorno });
+        return retorno;
+    }
+}
+
 exports = {
-    intercept: function(fn) {
-        return function() {
-            let retorno = fn.apply(this, arguments);
-            interceptado[fn.name]
-            if (!interceptado[fn.name]) {
-                interceptado[fn.name] = []
+    intercept: function(exportObject) {
+        const interceptedExport = {}
+        Object.keys(exportObject).forEach(function(key) {
+            if (typeof exportObject[key] === 'function') {
+                interceptedExport[key] = interceptFunction(exportObject[key]) 
+            } else {
+                interceptedExport[key] = exportObject[key] 
             }
-            interceptado[fn.name].push({ entrada: arguments, saida: retorno });
-            console.log(JSON.stringify(interceptado));
-            return retorno;
-        }
+        });
+        return interceptedExport;
     },
     save: function() {
-        console.log(fs.saveToFile)
         const fileContent = JSON.stringify(interceptado, null, 2)
         fs.saveToFile('interceptado.json', fileContent);
     }
--- test/test.js
+++ test/test.js
@@ -9,34 +9,45 @@ function somaAB(a, b) {
     return a + b;
 }
 
+let exportObject = {
+    maisUm: maisUm,
+    somaAB: somaAB
+}
+
+let exportIntercepted;
+function beforeAll() {
+   exportIntercepted = SnapTest.intercept(exportObject); 
+}
+
+function afterAll() {
+    SnapTest.save();
+}
+
 function exec (describe, it, beforeEach, afterEach, expect, should, assert) {
   afterEach(function () {
-    SnapTest.save();
   })
 
   beforeEach(function () {
-    // Função a ser executada após cada teste
   })
 
   describe('Teste Interceptor', function () {
     it('Interceptar funcao mais1', function () {
-      let interceptado = SnapTest.intercept(maisUm);
-      expect(interceptado(23)).to.equal(maisUm(23))
+      expect(exportIntercepted.maisUm(23)).to.equal(exportObject.maisUm(23))
     })
 
     it('Interceptar funcao somaAB', function () {
-      let interceptado = SnapTest.intercept(somaAB);
-      expect(interceptado(1, 2)).to.equal(somaAB(1, 2))
+      expect(exportIntercepted.somaAB(1, 2)).to.equal(exportObject.somaAB(1, 2))
     })
 
     it('Interceptar funcao mais1 novamente', function () {
-      let interceptado = SnapTest.intercept(maisUm);
-      expect(interceptado(13)).to.equal(maisUm(13))
+      expect(exportIntercepted.maisUm(13)).to.equal(exportObject.maisUm(13))
     })
   })
 }
 
+beforeAll()
 let res = majesty.run(exec)
+afterAll()
 
 print(res.success.length, ' scenarios executed with success and')
 print(res.failure.length, ' scenarios executed with failure.\n')
